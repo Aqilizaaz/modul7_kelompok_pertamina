21689c8bfe696bfeceb578313cfb4bd45827ad12
abrielkarisma <101442361+abrielkarisma@users.noreply.github.com>
2025-04-24 22:52:17 +0700

    desentralization1

--- modul7_kelompok_pertamina/modul7_kelompok_pertamina/DataMahasiswa_1201210018.cs
+++ modul7_kelompok_pertamina/modul7_kelompok_pertamina/DataMahasiswa_1201210018.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
-namespace modul7_kelompok_pertamina
+public class Address
 {
-    class DataMahasiswa_1201210018
+    public string streetAddress { get; set; }
+    public string city { get; set; }
+    public string state { get; set; }
+}
+
+public class Course
+{
+    public string code { get; set; }
+    public string name { get; set; }
+}
+
+public class Person
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string gender { get; set; }
+    public int age { get; set; }
+    public Address address { get; set; }
+    public List<Course> courses { get; set; }
+}
+
+public class DataMahasiswa_1201210018
+{
+    public void ReadJSON(string filePath)
     {
+     
+        string jsonContent = File.ReadAllText(filePath);
+        Person person = JsonSerializer.Deserialize<Person>(jsonContent);
+        
+        Console.WriteLine("=== DATA MAHASISWA ===");
+        Console.WriteLine($"Nama Lengkap : {person.firstName} {person.lastName}");
+        Console.WriteLine($"Jenis Kelamin: {person.gender}");
+        Console.WriteLine($"Usia         : {person.age} tahun");
+        Console.WriteLine("Alamat:");
+        Console.WriteLine($"  Jalan : {person.address.streetAddress}");
+        Console.WriteLine($"  Kota  : {person.address.city}");
+        Console.WriteLine($"  Prov  : {person.address.state}");
+        Console.WriteLine("Mata Kuliah:");
+        foreach (var course in person.courses)
+        {
+            Console.WriteLine($"  - {course.code} | {course.name}");
+        }
     }
 }
--- modul7_kelompok_pertamina/modul7_kelompok_pertamina/Program.cs
+++ modul7_kelompok_pertamina/modul7_kelompok_pertamina/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "jurnal7_1_1201210018.json"; 
+
+        DataMahasiswa_1201210018 data = new DataMahasiswa_1201210018();
+        data.ReadJSON(filePath);
+    }
+}
