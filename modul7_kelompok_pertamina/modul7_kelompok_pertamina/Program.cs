59a0db7ab1b2fe74cce073c0b3026f6bfd6e9fc8
Aqilizaaz <aqilizaaz16@gmail.com>
2025-04-24 23:08:56 +0700

    memperbaiki file path

--- modul7_kelompok_pertamina/modul7_kelompok_pertamina/Program.cs
+++ modul7_kelompok_pertamina/modul7_kelompok_pertamina/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        string filePath = "jurnal7_1_1201210018.json"; 
+        string filePath = "D:/pertamini/modul7_kelompok_pertamina/modul7_kelompok_pertamina/modul7_kelompok_pertamina/jurnal7_1_1201210018.json";
 
-        DataMahasiswa_1201210018 data = new DataMahasiswa_1201210018();
+    DataMahasiswa_1201210018 data = new DataMahasiswa_1201210018();
         data.ReadJSON(filePath);
     }
 }
