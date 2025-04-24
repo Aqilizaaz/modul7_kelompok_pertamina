ff5a590c705a368148a273c16a18ba48b922767a
abrielkarisma <101442361+abrielkarisma@users.noreply.github.com>
2025-04-24 23:27:00 +0700

    menambahkan class <TeamMembers >

---
+++ modul7_kelompok_pertamina/modul7_kelompok_pertamina/DataKelompok_1201210005.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+class 
+{
+
+public class TeamMembers
+{
+	public string firstName { get; set; }
+	public string lastName { get; set; }
+	public string gender { get; set; }
+	public int age { get; set; }
+	public string nim { get; set; }
+}
+
+public class Team
+{
+	public List<TeamMembers> members { get; set; }
+}
+
+public class DataTeamMember_1201210018
+{
+	public void ReadJSON(string filePath)
+	{
+		string jsonContent = File.ReadAllText(filePath);
+
+		Team team = JsonSerializer.Deserialize<Team>(jsonContent);
+
+		Console.WriteLine("Team member list:");
+		foreach (var member in team.members)
+		{
+			Console.WriteLine($"{member.nim} {member.firstName} {member.lastName} ({member.age} {member.gender})");
+		}
+	}
+}
+
+}	
--- modul7_kelompok_pertamina/modul7_kelompok_pertamina/Program.cs
+++ modul7_kelompok_pertamina/modul7_kelompok_pertamina/Program.cs
@@ -4,9 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        string filePath = "D:/pertamini/modul7_kelompok_pertamina/modul7_kelompok_pertamina/modul7_kelompok_pertamina/jurnal7_1_1201210018.json";
+        Console.WriteLine("Pilih menu:");
+        Console.WriteLine("1. Buka Data Mahasiswa");
+        Console.WriteLine("2. Buka Team Members");
+        Console.Write("Masukkan pilihan (1/2): ");
 
-    DataMahasiswa_1201210018 data = new DataMahasiswa_1201210018();
-        data.ReadJSON(filePath);
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                string filePath = "D:/pertamini/modul7_kelompok_pertamina/modul7_kelompok_pertamina/modul7_kelompok_pertamina/jurnal7_1_1201210018.json";
+                DataMahasiswa_1201210018 data = new DataMahasiswa_1201210018();
+                data.ReadJSON(filePath);
+                break;
+                
+            case "2":
+                string filePath2 = "D:/pertamini/modul7_kelompok_pertamina/modul7_kelompok_pertamina/modul7_kelompok_pertamina/jurnal7_2_1201210005.json";
+                TeamMembers_1201210005_TeguhSambadaAbrielKarisma data = new TeamMembers_1201210005_TeguhSambadaAbrielKarisma();
+                data.)ReadJSON(filePath2)
+                break;
+
+            default:
+                Console.WriteLine("Pilihan tidak valid.");
+                break;
+        }
+    }
+}
+
+public class TeamMembers_1201210005_TeguhSambadaAbrielKarisma
+{
+    public void DisplayTeamMembers()
+    {
+        Console.WriteLine("Team Members:");
+        Console.WriteLine("1. Teguh Sambada");
+        Console.WriteLine("2. Abriel Karisma");
     }
 }
